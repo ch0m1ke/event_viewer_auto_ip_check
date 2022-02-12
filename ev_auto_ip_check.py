158d426cf8dbd67068fab4a0671f8bf8dee6bbb1
iMik3 <48057851+ch0m1ke@users.noreply.github.com>
2022-02-12 17:46:28 +0100

    put main code in a function and fixed a bug with error message

--- ev_auto_ip_check.py
+++ ev_auto_ip_check.py
@@ -60,87 +60,91 @@ def color_print(text: str, color: str) -> bool:
     print(f"{font_colors[color]}{text}{font_colors['terminator']}")
     return True
 
-    
-if len(sys.argv) < 2:
-    file_name = input("File name: ")
-else:
-    file_name = sys.argv[1]
-# Open file and check entries
-try:
-    with open(file_name, mode='r', encoding='utf_8', newline='') as csvfile:
-        csv_header = [
-            'Receive Time',
-            'Severity',
-            'Event Type ID',
-            'Event Name',
-            'Device',
-            'Source',
-            'Source User Identity',
-            'Source Service',
-            'Destination Service',
-            'Destination',
-            'Destination FQDN',
-            'Action',
-            'Risk Rating',
-            'Description',
-            'Event ID',
-            'Source Interface',
-            'Destination Interface'
-        ]
-        ip_list = []
-        fw_check = csv.reader(csvfile, delimiter=',')
-        try:
-            if next(fw_check) == csv_header: # Check if file contains the right header
-                counter = 0
-                current_ip = ''
-                ip_info = []
-                for row in fw_check:
-                    if len(row) != len(csv_header) and row != []: # line doesn't match header
-                        color_print("\n[ERROR] The file may be corrupted!", "red")
-                        sys.exit()
-                    if len(row)!=0:
-                        if current_ip == '':
-                            current_ip = row[5]
-                            ip_info.append(row[0]) # Date of Event
-                            ip_info.append(row[5]) # Source IP
-                            ip_info.append(row[7]) # Source port
-                            ip_info.append(row[9]) # Destination IP
-                            ip_info.append(row[8]) # Destination port
-                        if row[5] == current_ip:
-                            counter+=1
-                        else:
-                            save_ip(ip_info[:], counter, ip_list)
-                            current_ip = row[5]
-                            ip_info[0] = row[0] # Date of Event
-                            ip_info[1] = row[5] # Source IP
-                            ip_info[2] = row[7] # Source port
-                            ip_info[3] = row[9] # Destination IP
-                            ip_info[4] = row[8] # Destination port
-                            counter = 1
-                save_ip(ip_info[:], counter, ip_list)
-                list_end = len(ip_list)
-                if len(ip_list)>4:
-                    list_end = 5 # Used to get the first 5 IPs
-                if len(ip_list)>0:
-                    ip_list.sort(key=lambda row: (row[-1]), reverse=True)
-                    clipboard = "These are the suspicious IPs that need to be verified on the external/internal router:\n\nShort situation regarding the source ip`s matching the search:\n"
-                    for entry in range(list_end):
-                        clipboard+=(f"\nSource IP: {ip_list[entry][1]} is seen {ip_list[entry][-1]} times in file uploaded!")
-                    for entry in range(list_end):
-                        clipboard+=(f"\n\nDate of Event: {ip_list[entry][0]}\n")
-                        clipboard+=(f"Source IP: {ip_list[entry][1]}\n")
-                        clipboard+=(f"Source port: {ip_list[entry][2]}\n")
-                        clipboard+=(f"Destination IP: {ip_list[entry][3]}\n")
-                        clipboard+=(f"Destination port: {ip_list[entry][4]}")
-                    copy_to_clipboard(clipboard)
-                    color_print(f"\n{clipboard}", "green")
-                    color_print("\nText copied to clipboard, lazy b*****d!", "yellow")
+def main():   
+    if len(sys.argv) < 2:
+        file_name = input("File name: ")
+    else:
+        file_name = sys.argv[1]
+    # Open file and check entries
+    try:
+        with open(file_name, mode='r', encoding='utf_8', newline='') as csvfile:
+            csv_header = [
+                'Receive Time',
+                'Severity',
+                'Event Type ID',
+                'Event Name',
+                'Device',
+                'Source',
+                'Source User Identity',
+                'Source Service',
+                'Destination Service',
+                'Destination',
+                'Destination FQDN',
+                'Action',
+                'Risk Rating',
+                'Description',
+                'Event ID',
+                'Source Interface',
+                'Destination Interface'
+            ]
+            ip_list = []
+            fw_check = csv.reader(csvfile, delimiter=',')
+            try:
+                if next(fw_check) == csv_header: # Check if file contains the right header
+                    counter = 0
+                    current_ip = ''
+                    ip_info = []
+                    for row in fw_check:
+                        if len(row) != len(csv_header) and row != []: # line doesn't match header
+                            color_print("\n[ERROR] The file may be corrupted!", "red")
+                            input("\nPress ENTER to quit...")
+                            sys.exit()
+                        if len(row)!=0:
+                            if current_ip == '':
+                                current_ip = row[5]
+                                ip_info.append(row[0]) # Date of Event
+                                ip_info.append(row[5]) # Source IP
+                                ip_info.append(row[7]) # Source port
+                                ip_info.append(row[9]) # Destination IP
+                                ip_info.append(row[8]) # Destination port
+                            if row[5] == current_ip:
+                                counter+=1
+                            else:
+                                save_ip(ip_info[:], counter, ip_list)
+                                current_ip = row[5]
+                                ip_info[0] = row[0] # Date of Event
+                                ip_info[1] = row[5] # Source IP
+                                ip_info[2] = row[7] # Source port
+                                ip_info[3] = row[9] # Destination IP
+                                ip_info[4] = row[8] # Destination port
+                                counter = 1
+                    save_ip(ip_info[:], counter, ip_list)
+                    list_end = len(ip_list)
+                    if len(ip_list)>4:
+                        list_end = 5 # Used to get the first 5 IPs
+                    if len(ip_list)>0:
+                        ip_list.sort(key=lambda row: (row[-1]), reverse=True)
+                        clipboard = "These are the suspicious IPs that need to be verified on the external/internal router:\n\nShort situation regarding the source ip`s matching the search:\n"
+                        for entry in range(list_end):
+                            clipboard+=(f"\nSource IP: {ip_list[entry][1]} is seen {ip_list[entry][-1]} times in file uploaded!")
+                        for entry in range(list_end):
+                            clipboard+=(f"\n\nDate of Event: {ip_list[entry][0]}\n")
+                            clipboard+=(f"Source IP: {ip_list[entry][1]}\n")
+                            clipboard+=(f"Source port: {ip_list[entry][2]}\n")
+                            clipboard+=(f"Destination IP: {ip_list[entry][3]}\n")
+                            clipboard+=(f"Destination port: {ip_list[entry][4]}")
+                        copy_to_clipboard(clipboard)
+                        color_print(f"\n{clipboard}", "green")
+                        color_print("\nText copied to clipboard, lazy b*****d!", "yellow")
+                    else:
+                        color_print("\n[ERROR] No records found!", "red")
                 else:
-                    color_print("\n[ERROR] No records found!", "red")
-            else:
-                color_print("\n[ERROR] The file may be corrupted!", "red")
-        except StopIteration:
-            color_print("\n[ERROR] The file is empty or corrupted!", "red")
-except OSError as err:
-    color_print("\n[ERROR] The file does't exist or can't be opened!", "red")
-input("\nPress ENTER to quit...")
+                    color_print("\n[ERROR] The file may be corrupted!", "red")
+            except StopIteration:
+                color_print("\n[ERROR] The file is empty or corrupted!", "red")
+    except OSError as err:
+        color_print("\n[ERROR] The file does't exist or can't be opened!", "red")
+    input("\nPress ENTER to quit...")
+
+if __name__ == '__main__':
+    main()
